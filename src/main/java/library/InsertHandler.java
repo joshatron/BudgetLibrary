c110b6e8aef04408b9a67b71f5e319e682571f6e
Joshua Leger <legermjoshua@gmail.com>
2017-08-30 09:25:43 -0700

    more stuff added for inserting

--- src/main/java/library/InsertHandler.java
+++ src/main/java/library/InsertHandler.java
@@ -1,6 +1,7 @@
 package library;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,14 +11,105 @@ import java.sql.Statement;
  */
 public class InsertHandler {
 
+    //todo: add duplicate testing
     public static boolean addTransaction(Transaction transaction, Connection conn) {
+        if(transaction.isValid()) {
+            int vendorID = getVendorID(transaction.getVendor(), conn);
+            if(vendorID == -1) {
+                addVendor(transaction.getVendor(), conn);
+                vendorID = getVendorID(transaction.getVendor(), conn);
+            }
+
+            String insert = "INSERT INTO transactions (timestamp, amount, vendor) " +
+                    "VALUES ( '" + transaction.getTimestamp().toString() + "', " +
+                    transaction.getAmount() + ", " +
+                    vendorID + " );";
+
+            try {
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(insert);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
         return false;
     }
 
+    private static int getVendorID(Vendor vendor, Connection conn) {
+        if(vendor.isValid()) {
+            String find = "SELECT id " +
+                    "FROM vendors " +
+                    "WHERE name = '" + vendor.getName() + "';";
+
+            try {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(find);
+
+                if(rs.next()) {
+                    return rs.getInt("id");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return -1;
+    }
+
+    //todo: add duplicate testing
     public static boolean addVendor(Vendor vendor, Connection conn) {
+        if(vendor.isValid()) {
+            int categoryID = getCategoryID(vendor.getCategory(), conn);
+            if(categoryID == -1) {
+                addCategory(vendor.getCategory(), conn);
+                categoryID = getCategoryID(vendor.getCategory(), conn);
+            }
+
+            String insertVendor = "INSERT INTO vendors (name, category) " +
+                    "VALUES ( '" + vendor.getName() + "', " + categoryID + " );";
+
+            try {
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(insertVendor);
+
+                int vendorID = getVendorID(vendor, conn);
+                for(String name : vendor.getRawNames()) {
+                    String insertName = "INSERT INTO vendor_namings (vendor_id, name) " +
+                            "VALUES ( " + vendorID + ", '" + name + "' );";
+                    stmt.executeUpdate(insertName);
+                }
+                //todo: add adding tags
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
         return false;
     }
 
+    private static int getCategoryID(Category category, Connection conn) {
+        if(category.isValid()) {
+            String find = "SELECT id " +
+                    "FROM categories " +
+                    "WHERE name = '" + category.getName() + "';";
+
+            try {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(find);
+
+                if(rs.next()) {
+                    return rs.getInt("id");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return -1;
+    }
+
+    //todo: add duplicate testing
     public static boolean addCategory(Category category, Connection conn) {
         if(category.isValid()) {
             String insert = "INSERT INTO categories (category, description, budget) " +
