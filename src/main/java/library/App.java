334feb5527c5d7cfde7c6d76bac67843093dc8fc
Joshua Leger <jleger@paypal.com>
2017-09-01 16:56:31 -0700

    Updates for the selecthandler and moved functions from inserthandler to selecthandler

--- src/main/java/library/App.java
+++ src/main/java/library/App.java
@@ -1,11 +1,20 @@
 package library;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         BudgetLibrary library = new BudgetLibrary();
 
         Category category = new Category("Rent", "rent", 2000);
-        Vendor vendor = new Vendor("River view", null, null, category);
+
+        ArrayList<String> tags = new ArrayList<String>();
+        tags.add("rent");
+        tags.add("subscriptions");
+        ArrayList<String> nicknames = new ArrayList<String>();
+        nicknames.add("RIVERVIEW APARTMENTS");
+        Vendor vendor = new Vendor("River view", nicknames, tags, category);
+
         Transaction transaction1 = new Transaction(new Timestamp("2017-08-01 10:00:00"), 2000, vendor);
         Transaction transaction2 = new Transaction(new Timestamp("2017-09-01 10:00:00"), 2000, vendor);
 
@@ -18,5 +27,4 @@ public class App {
         System.out.println();
         library.printCategories();
     }
-
 }
--- src/main/java/library/InsertHandler.java
+++ src/main/java/library/InsertHandler.java
@@ -13,16 +13,16 @@ public class InsertHandler {
 
     //add transaction, adding vendor if necessary
     public static boolean addTransaction(Transaction transaction, Connection conn) {
-        if(transaction.isValid()) {
+        if (transaction.isValid()) {
             //transaction already in database
-            if(getTransactionID(transaction, conn) != -1) {
+            if (SelectHandler.getTransactionID(transaction, conn) != -1) {
                 return false;
             }
-            int vendorID = getVendorID(transaction.getVendor(), conn);
+            int vendorID = SelectHandler.getVendorID(transaction.getVendor(), conn);
             //if vendor doesn't exist, add it
-            if(vendorID == -1) {
+            if (vendorID == -1) {
                 addVendor(transaction.getVendor(), conn);
-                vendorID = getVendorID(transaction.getVendor(), conn);
+                vendorID = SelectHandler.getVendorID(transaction.getVendor(), conn);
             }
 
             String insert = "INSERT INTO transactions (timestamp, amount, vendor) " +
@@ -44,17 +44,17 @@ public class InsertHandler {
 
     //add vendor, adding category if necessary
     public static boolean addVendor(Vendor vendor, Connection conn) {
-        if(vendor.isValid()) {
+        if (vendor.isValid()) {
             //vendor already in database
-            if(getVendorID(vendor, conn) != -1) {
+            if (SelectHandler.getVendorID(vendor, conn) != -1) {
                 return false;
             }
 
-            int categoryID = getCategoryID(vendor.getCategory(), conn);
+            int categoryID = SelectHandler.getCategoryID(vendor.getCategory(), conn);
             //category doesn't exist
-            if(categoryID == -1) {
+            if (categoryID == -1) {
                 addCategory(vendor.getCategory(), conn);
-                categoryID = getCategoryID(vendor.getCategory(), conn);
+                categoryID = SelectHandler.getCategoryID(vendor.getCategory(), conn);
             }
 
             String insertVendor = "INSERT INTO vendors (name, category) " +
@@ -65,10 +65,10 @@ public class InsertHandler {
                 Statement stmt = conn.createStatement();
                 stmt.executeUpdate(insertVendor);
 
-                int vendorID = getVendorID(vendor, conn);
+                int vendorID = SelectHandler.getVendorID(vendor, conn);
 
                 //add vendor raw names
-                if(vendor.getRawNames() != null) {
+                if (vendor.getRawNames() != null) {
                     for (String name : vendor.getRawNames()) {
                         String insertName = "INSERT INTO vendor_namings (vendor_id, name) " +
                                 "VALUES ( " + vendorID + ", '" + name + "' );";
@@ -77,15 +77,15 @@ public class InsertHandler {
                 }
 
                 //add vendor tags
-                if(vendor.getTags() != null) {
+                if (vendor.getTags() != null) {
                     for (String tag : vendor.getTags()) {
-                        int tagID = getTagID(tag, conn);
+                        int tagID = SelectHandler.getTagID(tag, conn);
 
                         if (tagID == -1) {
                             String insertTag = "INSERT INTO vendor_tags (name) " +
                                     "VALUES ( '" + tag + "' );";
                             stmt.executeUpdate(insertTag);
-                            tagID = getTagID(tag, conn);
+                            tagID = SelectHandler.getTagID(tag, conn);
                         }
 
                         String insertTagging = "INSERT INTO vendor_taggings (vendor_id, tag_id) " +
@@ -103,9 +103,9 @@ public class InsertHandler {
 
     //add category
     public static boolean addCategory(Category category, Connection conn) {
-        if(category.isValid()) {
+        if (category.isValid()) {
             //category already in database
-            if(getCategoryID(category, conn) != -1) {
+            if (SelectHandler.getCategoryID(category, conn) != -1) {
                 return false;
             }
 
@@ -123,95 +123,5 @@ public class InsertHandler {
         }
 
         return false;
-    }
-
-    private static int getTransactionID(Transaction transaction, Connection conn) {
-        if(transaction.isValid()) {
-            int vendorID = getVendorID(transaction.getVendor(), conn);
-
-            String find = "SELECT id " +
-                    "FROM transactions " +
-                    "WHERE timestamp = " + transaction.getTimestamp().getTimestampLong() + " AND " +
-                    "vendor = " + vendorID + ";";
-
-            try {
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(find);
-
-                if(rs.next()) {
-                    return rs.getInt("id");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return -1;
-    }
-
-    //find vendor id by searching for name
-    //if none found, return -1
-    private static int getVendorID(Vendor vendor, Connection conn) {
-        if(vendor.isValid()) {
-            String find = "SELECT id " +
-                    "FROM vendors " +
-                    "WHERE name = '" + vendor.getName() + "';";
-
-            try {
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(find);
-
-                if(rs.next()) {
-                    return rs.getInt("id");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return -1;
-    }
-
-    //find tag id by searching for name
-    //if none found, return -1
-    private static int getTagID(String tag, Connection conn) {
-        String find = "SELECT id " +
-                "FROM vendor_tags " +
-                "WHERE  name = '" + tag + "';";
-
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(find);
-
-            if(rs.next()) {
-                return rs.getInt("id");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return -1;
-    }
-
-    //find category id by searching for name
-    //if none found, return -1
-    private static int getCategoryID(Category category, Connection conn) {
-        if(category.isValid()) {
-            String find = "SELECT id " +
-                    "FROM categories " +
-                    "WHERE name = '" + category.getName() + "';";
-
-            try {
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(find);
-
-                if(rs.next()) {
-                    return rs.getInt("id");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return -1;
     }
 }
