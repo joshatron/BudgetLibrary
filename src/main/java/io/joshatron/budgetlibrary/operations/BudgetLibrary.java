39b2c71287732f2348fd5771aa825897e332d2f0
Joshua Leger <legermjoshua@gmail.com>
2019-03-19 21:53:09 -0700

    Started looking at this again

--- src/main/java/io/joshatron/budgetlibrary/operations/BudgetLibrary.java
+++ src/main/java/io/joshatron/budgetlibrary/operations/BudgetLibrary.java
@@ -1,36 +1,20 @@
 package io.joshatron.budgetlibrary.operations;
 
-import io.joshatron.budgetlibrary.database.DatabaseManager;
 import io.joshatron.budgetlibrary.objects.Transaction;
-import io.joshatron.budgetlibrary.objects.Vendor;
-
-import java.sql.Connection;
 
 /*
  * Wrapper for all the budget classes
- * The goal of this class is to be the univeral api
+ * The goal of this class is to be the universal api
  */
 public class BudgetLibrary {
 
-    private Connection conn;
-
     public BudgetLibrary() {
-        this.conn = DatabaseManager.getConnection();
     }
 
-    public boolean addTransaction(Transaction transaction) {
-        return false;
+    public void addTransaction(Transaction transaction) {
     }
 
-    public boolean addTransactions(Transaction[] transactions) {
-        return false;
+    public void addTransactions(Transaction[] transactions) {
     }
 
-    public boolean addVendor(Vendor vendor) {
-        return false;
-    }
-
-    public boolean addVendors(Vendor[] vendors) {
-        return false;
-    }
 }
