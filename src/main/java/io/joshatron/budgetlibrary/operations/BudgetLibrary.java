b35de3e7313891b5e0d2252619c83b1c43f080cc
Joshua Leger <legermjoshua@gmail.com>
2019-04-08 21:17:48 -0700

    Started building up budget library class

--- src/main/java/io/joshatron/budgetlibrary/operations/BudgetLibrary.java
+++ src/main/java/io/joshatron/budgetlibrary/operations/BudgetLibrary.java
@@ -1,20 +1,26 @@
 package io.joshatron.budgetlibrary.operations;
 
-import io.joshatron.budgetlibrary.dtos.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 /*
  * Wrapper for all the budget classes
  * The goal of this class is to be the universal api
  */
 public class BudgetLibrary {
+    private Session session;
 
     public BudgetLibrary() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+        session = factory.openSession();
     }
 
-    public void addTransaction(Transaction transaction) {
+    public BudgetLibrary(Session session) {
+        this.session = session;
     }
-
-    public void addTransactions(Transaction[] transactions) {
-    }
-
 }
