4343e146ea2abb29e1195f0f9d72d4ae87915eb3
Joshua Leger <legermjoshua@gmail.com>
2019-04-09 17:10:41 -0700

    Replaced transaction dao to use hibernate

--- src/main/java/io/joshatron/budgetlibrary/database/TransactionDAO.java
+++ src/main/java/io/joshatron/budgetlibrary/database/TransactionDAO.java
@@ -2,14 +2,49 @@ package io.joshatron.budgetlibrary.database;
 
 import io.joshatron.budgetlibrary.dtos.*;
 import io.joshatron.budgetlibrary.exception.BudgetLibraryException;
+import org.hibernate.Session;
 
 import java.util.List;
 
-public interface TransactionDAO {
+public class TransactionDAO {
 
-    Transaction createTransaction(Timestamp timestamp, Money amount, Vendor vendor, Account account) throws BudgetLibraryException;
-    void updateTransaction(int transactionId, Transaction newTransaction) throws BudgetLibraryException;
-    void deleteTransaction(int transactionId) throws BudgetLibraryException;
+    public static Transaction createTransaction(Session session, Timestamp timestamp, Money amount, Vendor vendor, Account account) throws BudgetLibraryException {
+        org.hibernate.Transaction tx = session.beginTransaction();
 
-    List<Transaction> getTransactions(Timestamp start, Timestamp end, Money min, Money max, Vendor vendor, Account account, Type type) throws BudgetLibraryException;
+        Transaction transaction = new Transaction();
+        transaction.setTimestamp(timestamp);
+        transaction.setAmount(amount);
+        transaction.setVendor(vendor);
+        transaction.setAccount(account);
+        session.save(transaction);
+
+        tx.commit();
+
+        return transaction;
+    }
+
+    public static void updateTransaction(Session session, int transactionId, Transaction newTransaction) throws BudgetLibraryException {
+        org.hibernate.Transaction tx = session.beginTransaction();
+
+        Transaction transaction = session.get(Transaction.class, transactionId);
+        transaction.setTimestamp(newTransaction.getTimestamp());
+        transaction.setAmount(newTransaction.getAmount());
+        transaction.setVendor(newTransaction.getVendor());
+        transaction.setAccount(newTransaction.getAccount());
+
+        tx.commit();
+    }
+
+    public static void deleteTransaction(Session session, int transactionId) throws BudgetLibraryException {
+        org.hibernate.Transaction tx = session.beginTransaction();
+
+        Transaction transaction = session.get(Transaction.class, transactionId);
+        session.delete(transaction);
+
+        tx.commit();
+    }
+
+    public static List<Transaction> getTransactions(Session session, Timestamp start, Timestamp end, Money min, Money max, Vendor vendor, Account account, Type type) throws BudgetLibraryException {
+        return null;
+    }
 }
