b9846df75e93228ad0aa65fcc199f703be42590d
Joshua Leger <legermjoshua@gmail.com>
2019-08-14 19:32:21 -0700

    New analysis object

---
+++ src/main/java/io/joshatron/budgetlibrary/analysis/TimeframeAnalysis.java
@@ -0,0 +1,19 @@
+package io.joshatron.budgetlibrary.analysis;
+
+import io.joshatron.budgetlibrary.dtos.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class TimeframeAnalysis {
+    private List<Transaction> transactions;
+    private Money totalIn;
+    private Money totalOut;
+    private Map<Vendor,Money> spendingPerVendor;
+    private Map<Type,Money> spendingPerType;
+    private Map<Timestamp,Money> spendingPerDay;
+}
