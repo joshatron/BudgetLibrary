751f41601bb45f78059005da894a6397d9f09863
Joshua Leger <legermjoshua@gmail.com>
2020-09-18 20:26:14 -0700

    Created test sequence to work with

--- test/budget_library/analysis_test.clj
+++ test/budget_library/analysis_test.clj
@@ -2,7 +2,22 @@
   (:require [clojure.test :refer :all]
             [budget-library.import :as i]
             [budget-library.analysis :as a]
-            [java-time :as t]))
+            [java-time :as t])
+  (:import (java.util UUID)))
+
+(def repeating-transactions (iterate (fn [v] {:id          (.toString (UUID/randomUUID))
+                                              :date        (t/plus (:date v) (t/days 1))
+                                              :description (:description v)
+                                              :amount      (+ 100 (:amount v))
+                                              :partner     (.toString (UUID/randomUUID))
+                                              :tags        (:tags v)
+                                              })
+                                     {:id          (.toString (UUID/randomUUID))
+                                      :date        (t/local-date-time 2020 3 30)
+                                      :description "Description."
+                                      :amount      100
+                                      :partner     (.toString (UUID/randomUUID))
+                                      :tags        #{:tag1 :tag2 :tag3}}))
 
 (deftest ending-on-test
   (testing "Empty list gets nil"
