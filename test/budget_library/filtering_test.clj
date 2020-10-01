36277a59315de8d71d7671fe46153e5d7f66ab7d
Joshua Leger <legermjoshua@gmail.com>
2020-09-30 21:27:24 -0700

    Tags change predicatably now

--- test/budget_library/filtering_test.clj
+++ test/budget_library/filtering_test.clj
@@ -5,19 +5,27 @@
             [java-time :as t])
   (:import (java.util UUID)))
 
+(defn- next-tag [tag]
+  (keyword (str "tag" (inc (Integer/parseInt (subs (name tag) 3))))))
+
+(defn- next-tags [tags]
+  (set (map next-tag tags)))
+
 (def daily-transactions (iterate (fn [v] {:id          (.toString (UUID/randomUUID))
                                               :date        (t/plus (:date v) (t/days 1))
                                               :description (:description v)
                                               :amount      (+ 100 (:amount v))
                                               :partner     (str (inc (Integer/parseInt (:partner v))))
-                                              :tags        (:tags v)
+                                              :tags        (next-tags (:tags v))
                                               })
                                      {:id          (.toString (UUID/randomUUID))
                                       :date        (t/local-date-time 2020 3 30)
                                       :description "Description."
                                       :amount      -1000
                                       :partner     "1"
-                                      :tags        #{:tag1 :tag2 :tag3}}))
+                                      :tags        #{:tag1 :tag2}}))
+
+(println (take 3 daily-transactions))
 
 (deftest ending-on-test
   (testing "Empty list gets nil"
