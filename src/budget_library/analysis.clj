2304f0d37c1b36014e03aa29aea8a4f5e3d37bc0
Joshua Leger <legermjoshua@gmail.com>
2020-10-05 09:12:16 -0700

    Added net amount for analysis

--- src/budget_library/analysis.clj
+++ src/budget_library/analysis.clj
@@ -1,1 +1,6 @@
 (ns budget-library.analysis)
+
+(defn net-amount
+  "Gets the net amount of money spent/received for the given transactions"
+  [transactions]
+  (reduce #(+ %1 (:amount %2)) 0 transactions))
--- test/budget_library/analysis_test.clj
+++ test/budget_library/analysis_test.clj
@@ -2,3 +2,13 @@
   (:require [clojure.test :refer :all]
             [budget-library.test-utils :refer :all]
             [budget-library.analysis :as a]))
+
+(deftest net-amount-test
+  (testing "Empty list gets 0"
+    (is (= 0 (a/net-amount '()))))
+  (testing "All negatives get combined negative amount"
+    (is (= -3400 (a/net-amount (take 4 daily-transactions)))))
+  (testing "Equal amounts positive and negative net at 0"
+    (is (= 0 (a/net-amount (take 21 daily-transactions)))))
+  (testing "Only positive amounts combine to positive amount"
+    (is (= 5500 (a/net-amount (nthrest (take 21 daily-transactions) 10))))))
