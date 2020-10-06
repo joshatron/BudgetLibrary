ea665b3e33f91ee3c6b4845eb24d89f8ebea4fbf
Joshua Leger <legermjoshua@gmail.com>
2020-10-05 21:37:28 -0700

    Added total positive function

--- src/budget_library/analysis.clj
+++ src/budget_library/analysis.clj
@@ -4,3 +4,8 @@
   "Gets the net amount of money spent/received for the given transactions"
   [transactions]
   (reduce #(+ %1 (:amount %2)) 0 transactions))
+
+(defn total-positive
+  "Adds up all the transaction amounts that are positive"
+  [transactions]
+  (net-amount (filter #(pos? (:amount %)) transactions)))
--- test/budget_library/analysis_test.clj
+++ test/budget_library/analysis_test.clj
@@ -12,3 +12,13 @@
     (is (= 0 (a/net-amount (take 21 daily-transactions)))))
   (testing "Only positive amounts combine to positive amount"
     (is (= 5500 (a/net-amount (nthrest (take 21 daily-transactions) 10))))))
+
+(deftest total-positive-test
+  (testing "Empty list gets 0"
+    (is (= 0 (a/total-positive '()))))
+  (testing "All positive gets combined amount"
+    (is (= 600 (a/total-positive (nthrest (take 14 daily-transactions) 11)))))
+  (testing "All negative gets 0"
+    (is (= 0 (a/total-positive (take 7 daily-transactions)))))
+  (testing "Mix of positive and negative only counts positive"
+    (is (= 600 (a/total-positive (take 14 daily-transactions))))))
