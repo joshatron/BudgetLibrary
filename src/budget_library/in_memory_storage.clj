057d1c9c9f26a6530813202f31bfb7eb058f0b45
Joshua Leger <legermjoshua@gmail.com>
2020-10-11 10:54:11 -0700

    Started looking at a simple in memory storage implementation

---
+++ src/budget_library/in_memory_storage.clj
@@ -0,0 +1,4 @@
+(ns budget-library.in-memory-storage
+  (:require [budget-library.storage :as s]))
+
+
---
+++ test/budget_library/in_memory_storage_test.clj
@@ -0,0 +1,8 @@
+(ns budget-library.in-memory-storage-test
+  (:require [clojure.test :refer :all]
+            [budget-library.test-utils :refer :all]
+            [budget-library.import :as i]
+            [budget-library.storage :as s]))
+
+
+
